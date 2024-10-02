7b79f4c477465e60218600448219fcbd0cd046ce
Jack Buss <jack.buss@socialworkengland.org.uk>
2024-10-02 16:20:18 +0100

    [+] XML documentation for IAuthenticationController

--- Server/ServiceManager/Interfaces/Authentication/IAuthenticationController.cs
+++ Server/ServiceManager/Interfaces/Authentication/IAuthenticationController.cs
@@ -8,9 +8,26 @@ namespace Server.ServiceManager.Interfaces.Authentication
 {
     internal interface IAuthenticationController
     {
+        /// <summary>
+        /// Log in with username and password
+        /// </summary>
+        /// <param name="username">Usually the serial number</param>
+        /// <param name="password">A six-digit code generated using generatePasswordForUsername</param>
+        /// <returns></returns>
         public string loginWithUsernamePassword(string username, string password);
 
+        /// <summary>
+        /// The one-time-passcode generation for smart meter onboarding
+        /// </summary>
+        /// <param name="username">Meter serial number</param>
+        /// <returns></returns>
         public string generatePasswordForUsername(string username);
+
+        /// <summary>
+        /// Invalidate the one-time-passcode for a meter
+        /// </summary>
+        /// <param name="username">Meter serial number</param>
+        /// <returns></returns>
         public bool invalidatePasswordForUsername(string username);
     }
 }
