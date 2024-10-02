43d8b6418d499bd014aa4766dc0bf289fada52b3
Jack Buss <jack.buss@socialworkengland.org.uk>
2024-10-02 16:12:56 +0100

    [*] Added XML docs to IReadingController

--- Client/ServiceManager/Interfaces/Controllers/Communication/IReadingController.cs
+++ Client/ServiceManager/Interfaces/Controllers/Communication/IReadingController.cs
@@ -11,8 +11,21 @@ namespace Client.ServiceManager.Interfaces.Controllers.Communication
 {
     public interface IReadingController
     {
+        /// <summary>
+        /// Sends the reading to the server, to be used by the thread manager to run asynchronously.
+        /// </summary>
         public void SendReading();
+
+        /// <summary>
+        /// Sets the model via another thread which is periodicallu pulled by the SendReading process to be sent to server.
+        /// </summary>
+        /// <param name="cdm">ClientDataModel queued to send to server</param>
         public void setClientDataModel(ClientDataModel cdm);
+
+        /// <summary>
+        /// Updates the callback function for a frontend component that logs all traffic.
+        /// </summary>
+        /// <param name="callback">Usually a RichTextBox</param>
         public void setRichTextBox(Action<string> callback);
     }
 }
