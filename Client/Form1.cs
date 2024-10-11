1cbcdb03ccdf6d2ecd6eee3d26496f037a283a5a
Todd Longbottom <toddmarcusl17@outlook.com>
2024-10-11 18:45:06 +0100

    UI elements added

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,26 +1,43 @@
 using Client.Controllers.Communication;
 using Client.Models.Communication;
 using Client.ServiceManager.Interfaces.Controllers.Communication;
+using MindFusion.Gauges;
+using System.Data;
 
 namespace Client
 {
     public partial class Form1 : Form
     {
         IReadingController readingController;
+        float electricityUsage;
+        decimal electricityUsageDec;
         public Form1(IReadingController rc)
         {
             readingController = rc;
             InitializeComponent();
 
+            Random rng = new Random();
+            electricityUsage = rng.Next(5, 30);
+            electricityUsageDec = Convert.ToDecimal(electricityUsage);
+
             readingController.SetClientDataModel(new()
             {
                 Id = Random.Shared.Next(),
                 LocationId = 2,
-                ElectricityUsage = 23,
+                ElectricityUsage = electricityUsageDec,
                 ConnectionDateAndTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ")
             });
 
             readingController.SendReading();
+
+            #region GAUGE LOGIC
+            today_Gauge.Scales[0].Ranges[0].MaxValue = electricityUsage;
+            today_Gauge.Scales[0].Pointers[0].Value = electricityUsage;
+            //if(electricityUsage <= 23 && electricityUsage >= 15)
+            // {
+            //}
+
+            #endregion
         }
 
         public void receivedReading(string reading)
@@ -41,7 +58,7 @@ namespace Client
             {
                 Id = Random.Shared.Next(),
                 LocationId = 2,
-                ElectricityUsage = 23,
+                ElectricityUsage = electricityUsageDec,
                 ConnectionDateAndTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ")
             });
 
@@ -51,6 +68,21 @@ namespace Client
         private void Form1_Load(object sender, EventArgs e)
         {
             RichTextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void ovalGauge1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
