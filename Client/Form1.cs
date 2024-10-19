4071b578705255b8d0beafe159bde1ead3643532
Luke <Luke.Marshall@kerridgecs.com>
2024-10-19 16:52:40 +0100

    .

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,8 +1,4 @@
-using Client.Controllers.Communication;
-using Client.Models.Communication;
 using Client.ServiceManager.Interfaces.Controllers.Communication;
-using MindFusion.Gauges;
-using System.Data;
 
 namespace Client
 {
@@ -29,13 +25,13 @@ namespace Client
             amber = today_Gauge.Scales[0].Ranges[1].Fill;
             red = today_Gauge.Scales[0].Ranges[2].Fill;
 
-            readingController.SetClientDataModel(new()
-            {
-                Id = Random.Shared.Next(),
-                LocationId = 2,
-                ElectricityUsage = electricityUsageDec,
-                ConnectionDateAndTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ")
-            });
+            //readingController.SetClientDataModel(new()
+            //{
+            //    Id = Random.Shared.Next(),
+            //    LocationId = 2,
+            //    ElectricityUsage = electricityUsageDec,
+            //    ConnectionDateAndTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            //});
 
             //readingController.SendReading();
 
